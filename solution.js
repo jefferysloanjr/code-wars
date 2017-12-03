772e178da8beb0b5642b93c3ca83fd65f939f872
Jeffery Sloan <jefferysloanjr@gmail.com>
2017-12-03 13:35:41 -0800

    validate pin code

--- solution.js
+++ solution.js
@@ -1,8 +1,8 @@
 'use strict';
 
-// solution for validate username with green passing.
+// solution for validate pin code with green passing.
 
-function validateUsr(username) {
-  res = /^[a-z0-9_]{4,16}$/g.test(username)
-  return res
+function validatePIN (pin) {
+  return /^(\d{4}|\d{6})$/.test(pin);
+  //return true or false
 }
