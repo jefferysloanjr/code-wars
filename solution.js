ce1daae80732b08f7346249ba79d349edccb7d39
Jeffery Sloan <jefferysloanjr@gmail.com>
2017-12-06 08:10:37 -0800

    disemvowel finished

--- solution.js
+++ solution.js
@@ -1,15 +1,7 @@
 'use strict';
 
-// solution for nice array with green passing.
+// solution for disemvowel with green passing.
 
-function isNice(arr){
-let flag = true;
-if (arr.length<1){
-  flag = false}
- arr.forEach(function(i) {
-    if ((arr.includes(i + 1))||(arr.includes(i - 1))) {
-  } else {
-    flag = false}
-    })
-  return flag
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, '');
 }
