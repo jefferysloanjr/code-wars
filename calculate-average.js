0a61dae6f090a630d7fe43da2b9f288d4d9fe6da
Jeffery Sloan <jefferysloanjr@gmail.com>
2017-12-12 08:15:23 -0800

    finished odd numbers

--- calculate-average.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// solution for list filtering with green passing.
-function find_average(array) {
-  // your code here
-  return array.reduce((sum, num)=> sum + num) / array.length;
-}
